045d74cee45f4ee97e718f1d97f7108a73079755
Eugene <36643057+Thaseugene@users.noreply.github.com>
2018-02-24 18:06:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var array=[];
+    var res = {};
+    var H,D,Q,N,P;
+    if (currency>10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    if (currency<=0) {
+        return {}
+    }
+    if (currency>1000) {
+        H=Math.floor((currency-1)/50);
+        Q=Math.floor(((currency-1)%50)/25);
+        D=Math.floor((((currency-1)%50)%25)/10);
+        N=Math.floor(((((currency-1)%50)%25)%10)/5);
+        P=Math.floor((((((currency-1)%50)%25)%10)%5)/1)+1;
+     
+        var result = {H: H, Q: Q, D: D, N: N, P: P};
+        for (var key in result) {
+            if (result[key]>0) {
+                console.log(result[key]);
+                res[key]=result[key];
+            }   
+        } 
+        return res;
+    }
+    if (currency<=1000) {
+        H=Math.floor(currency/50);
+        Q=Math.floor((currency%50)/25);
+        D=Math.floor(((currency%50)%25)/10);
+        N=Math.floor((((currency%50)%25)%10)/5);
+        P=Math.floor(((((currency%50)%25)%10)%5)/1);
+
+        var result = {H: H, Q: Q, D: D, N: N, P: P};
+        for (var key in result) {
+            //var value=result[key];
+            if (result[key]>0) {
+                console.log(result[key]);
+                res[key]=result[key];
+            }   
+        } 
+        return res;
+    }
 }
